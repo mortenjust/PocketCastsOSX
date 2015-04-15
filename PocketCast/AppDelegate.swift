b9add4507c55c4e736a200ad59079d4e83d7b425
mortenjust <morten.just@genstart.dk>
2015-04-14 20:41:34 -0700

    Initial Commit

---
+++ PocketCast/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PocketCast
+//
+//  Created by Morten Just Petersen on 4/14/15.
+//  Copyright (c) 2015 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
