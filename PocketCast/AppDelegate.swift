f927b255b92d044c0bd1083bba7fddb1f595e7f9
Bill <bpenda94@gmail.com>
2016-11-09 19:07:52 -0600

    swift 3 conversion

--- PocketCast/AppDelegate.swift
+++ PocketCast/AppDelegate.swift
@@ -21,16 +21,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
 
     override init() {
         // Register defaults for the whitelist of apps that want to use media keys
-        NSUserDefaults.standardUserDefaults().registerDefaults(
-            [kMediaKeyUsingBundleIdentifiersDefaultsKey : SPMediaKeyTap.defaultMediaKeyUserBundleIdentifiers()])
+        UserDefaults.standard.register(
+            defaults: [kMediaKeyUsingBundleIdentifiersDefaultsKey : SPMediaKeyTap.defaultMediaKeyUserBundleIdentifiers()])
     }
 
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let window = NSApplication.sharedApplication().windows.first!
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let window = NSApplication.shared().windows.first!
         window.titlebarAppearsTransparent = true
         window.title = ""
-        window.movableByWindowBackground = true
+        window.isMovableByWindowBackground = true
         window.backgroundColor = NSColor(red: CGFloat(0xf4)/CGFloat(0xff), green: CGFloat(0x43)/CGFloat(0xff), blue: CGFloat(0x36)/CGFloat(0xff), alpha: 1.0)
         
         // let repFileName = window.representedFilename
@@ -42,10 +42,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
         window.setFrameAutosaveName(repFileName)
         window.windowController?.shouldCascadeWindows = false
         
-        window.releasedWhenClosed = false
+        window.isReleasedWhenClosed = false
 
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "gotNotification:", name: "pocketEvent", object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.gotNotification(_:)), name: NSNotification.Name(rawValue: "pocketEvent"), object: nil)
 
         webView.mainFrameURL = "https://play.pocketcasts.com/"
         webView.policyDelegate = self
@@ -57,29 +57,29 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
         }
     }
     
-    @IBAction func newWindow(sender: NSMenuItem) {
+    @IBAction func newWindow(_ sender: NSMenuItem) {
         window.makeKeyAndOrderFront(sender)
     }
     
-    @IBAction func reloadPage(sender: AnyObject) {
+    @IBAction func reloadPage(_ sender: AnyObject) {
         webView.reload(sender)
     }
     
-    func webView(webView: WebView!, decidePolicyForNewWindowAction actionInformation: [NSObject : AnyObject]!, request: NSURLRequest!, newFrameName frameName: String!, decisionListener listener: WebPolicyDecisionListener!) {
-        NSWorkspace.sharedWorkspace().openURL(request.URL!)
+    func webView(_ webView: WebView!, decidePolicyForNewWindowAction actionInformation: [AnyHashable: Any]!, request: URLRequest!, newFrameName frameName: String!, decisionListener listener: WebPolicyDecisionListener!) {
+        NSWorkspace.shared().open(request.url!)
     }
 
-    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false;
     }
     
-    func applicationShouldHandleReopen(_sender: NSApplication,
+    func applicationShouldHandleReopen(_ _sender: NSApplication,
                                          hasVisibleWindows flag: Bool) -> Bool{
         window.setIsVisible(true)
         return true
     }
 
-    func gotNotification(notification : NSNotification){
+    func gotNotification(_ notification : Notification){
 		if let userInfo = notification.userInfo as? Dictionary<String,String> {
 			if let action = userInfo["action"] {
 				print("Got Notification \(action)")
@@ -87,16 +87,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
 				
 				switch(action) {
 					case "playPause":
-						webView.stringByEvaluatingJavaScriptFromString(
-							"\(angularMediaPlayerSelector).playPause()")
+						webView.stringByEvaluatingJavaScript(
+							from: "\(angularMediaPlayerSelector).playPause()")
 					
 					case "skipForward":
-						webView.stringByEvaluatingJavaScriptFromString(
-							"\(angularMediaPlayerSelector).jumpForward()")
+						webView.stringByEvaluatingJavaScript(
+							from: "\(angularMediaPlayerSelector).jumpForward()")
 					
 					case "skipBack":
-						webView.stringByEvaluatingJavaScriptFromString(
-							"\(angularMediaPlayerSelector).jumpBack()")
+						webView.stringByEvaluatingJavaScript(
+							from: "\(angularMediaPlayerSelector).jumpBack()")
 					
 					default:
 						break
@@ -105,19 +105,19 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
 		}
     }
 
-    func applicationDockMenu(sender: NSApplication) -> NSMenu? {
+    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
         let menu = NSMenu(title: "Play Control")
-        let item = NSMenuItem(title: "Play/Pause", action: "playPause", keyEquivalent: "P")
+        let item = NSMenuItem(title: "Play/Pause", action: #selector(AppDelegate.playPause), keyEquivalent: "P")
         menu.addItem(item)
         return menu
     }
     
     func playPause(){
-        NSNotificationCenter.defaultCenter().postNotificationName(
-            "pocketEvent", object:NSApp, userInfo:["action":"playPause"])
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "pocketEvent"), object:NSApp, userInfo:["action":"playPause"])
     }
     
-    override func mediaKeyTap(mediaKeyTap : SPMediaKeyTap?, receivedMediaKeyEvent event : NSEvent) {
+    override func mediaKeyTap(_ mediaKeyTap : SPMediaKeyTap?, receivedMediaKeyEvent event : NSEvent) {
 
         let keyCode = Int((event.data1 & 0xFFFF0000) >> 16);
         let keyFlags = (event.data1 & 0x0000FFFF);
@@ -129,12 +129,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
                     playPause()
 
                 case Int(NX_KEYTYPE_FAST):
-                    NSNotificationCenter.defaultCenter().postNotificationName(
-                        "pocketEvent", object: NSApp, userInfo:["action":"skipForward"])
+                    NotificationCenter.default.post(
+                        name: Notification.Name(rawValue: "pocketEvent"), object: NSApp, userInfo:["action":"skipForward"])
 
                 case Int(NX_KEYTYPE_REWIND):
-                    NSNotificationCenter.defaultCenter().postNotificationName(
-                        "pocketEvent", object: NSApp, userInfo:["action":"skipBack"])
+                    NotificationCenter.default.post(
+                        name: Notification.Name(rawValue: "pocketEvent"), object: NSApp, userInfo:["action":"skipBack"])
 
                 default:
                     break
@@ -142,7 +142,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, WebPolicyDelegate {
         }
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
